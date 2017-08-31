9f01a9d6f7f72c711c231ec1e99ad24ec7989324
yollysparks <yolanda.owiso.yo@gmail.com>
2017-08-31 20:56:06 +0200

    Merge origin/master
    Conflicts:
    	pom.xml
    	src/main/java/Controller/ConsumerServlet.java
    	src/main/java/Controller/CustomerServlet.java
    	src/main/java/Data/CustomerMapper.java
    	src/main/java/testing.java
    	src/main/webapp/WEB-INF/web.xml
    	src/test/java/Data/CustomerMapperTest.java

---
+++ src/main/webapp/app.js
@@ -0,0 +1,16 @@
+function evhandler(){
+var promise = fetch('https://jokes-plaul.rhcloud.com/api/joke');
+     promise.then(function (response) {
+       return response.json();
+     }).then(function (quote) {
+        
+        
+                                                          
+     });
+ }
+ document.getElementById("h4").onclick = evhandler;
+ 
+//        document.getElementById("joke").innerText = quote.joke;
+//        document.getElementById("id").innerText = quote.id;
+//        document.getElementById("ref").innerText = quote.reference;
+
